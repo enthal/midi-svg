14036dc47d25469876b6657d1c7eeb78f8e0b392
Tim James <tjames@tjames.com>
2016-08-06 20:52:05 -0700

    lil cleaner

--- index.js
+++ index.js
@@ -10,31 +10,17 @@ const log = console.log;
 const toJsonInd = x => JSON.stringify(x,0,2);
 const logJson = x => log(toJsonInd(x));
 
-log(MIDIUtils.noteNameToNoteNumber('A-4'));
-log(MIDIUtils.noteNumberToName( 21 ));
-log(MIDIUtils.noteNumberToName( 22 ));
-log(MIDIUtils.noteNumberToName( 23 ));
-
-
 // const midiSong = (await got("https://s3.amazonaws.com/www.tjames.com/levels_midi.mid")).body
 const midiBlob = fs.readFileSync('levels.mid', 'binary');
-// const song = midiConverter.midiToJson(midiBlob);  // not json
 const song = midiFileParser(midiBlob);
 
 log(song.tracks[0].length)
 
-let t = 0;
-_(song.tracks[0])
-  .filter(x => x.type == 'channel')
-  // .each(x => (x.t = t += x.deltaTime))
-  .map(x => [t += x.deltaTime, MIDIUtils.noteNumberToName(x.noteNumber), x.velocity || null ])
-  .each(x => log(x.join('\t')))
-  // .each()
 
 let activeNotesByNumber = {};
 let notes = [];
 let time = 0;
-_.each(song.tracks[0], event=>{
+_.each(song.tracks[0], event => {
   if (event.type !== 'channel') return;
   // log(MIDIUtils.noteNumberToName(event.noteNumber))
   time += event.deltaTime;
@@ -53,8 +39,8 @@ _.each(song.tracks[0], event=>{
     delete activeNotesByNumber[event.noteNumber];  // needless
   }
 });
-logJson(notes);
+// logJson(notes);
 _.each(notes, note => log([note.time.start, note.time.duration, note.number, note.name].join('\t')));
-logJson(notes.length);
+log(notes.length);
 // logJson(activeNotesByNumber);
 // console.log(JSON.stringify(song,0,2));
